42de8a1c47e8455ea92192f0f06ddefec036d725
Emil Lorentzen <emilerher@gmail.com>
2023-05-12 08:05:29 +0200

    Add project files.

---
+++ I for SOLID/ISlash.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace I_for_SOLID
+{
+    interface ISlash
+    {
+        void Slash();
+    }
+}
---
+++ I for SOLID/Witch.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace I_for_SOLID
+{
+    public class Witch : IDie, IFight, IHeal, IRaiseShield, IShieldGlare, ITeleport
+    {
+        public void Die()
+        {
+            Console.WriteLine("I'm slashing someone");
+        }
+
+        public void Fight()
+        {
+            Console.WriteLine("I'm fighting");
+        }
+
+        public void Heal()
+        {
+            Console.WriteLine("I'm healing");
+        }
+
+        public void RaiseShield()
+        {
+            Console.WriteLine("I'm raising my shield");
+        }
+
+        public void ShieldGlare()
+        {
+            Console.WriteLine("I'm throwing shield glare");
+        }
+
+        public void Teleport(int x, int y)
+        {
+            Console.WriteLine("I'm teleporting to " + x + " " + y);
+        }
+    }
+}
